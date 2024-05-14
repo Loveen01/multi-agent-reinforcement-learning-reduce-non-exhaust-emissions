db20700e6e62e90402d7bb8a48e337a90c6aabaf
loveen011 <loveenomar05@gmail.com>
2024-05-14 22:46:35 +0100

    train with 0.4 on Azure

--- train_experiment_azure.py
+++ train_experiment_azure.py
@@ -12,13 +12,17 @@ from azureml.tensorboard import Tensorboard
 
 def main():
     ws = Workspace.from_config(path='azure_config.json')
-    experiment_name = "4x4grid_env"
+    experiment_name = "4x4grid_env_resco_train_with_alpha_0.4"
     compute_name = 'ray-cluster-cpu'
 
     # Run infra/cluster.py for creating the cluster
     compute_target = ws.compute_targets[compute_name]
-    rayEnv = Environment.from_pip_requirements(name = "RLEnv",
-                                               file_path = "requirements.txt")
+
+    rayEnv = Environment.from_dockerfile(name="RLEnv", 
+                                         dockerfile="dockerfile",
+                                         conda_specification="requirements.yml")
+    # rayEnv = Environment.from_pip_requirements(name = "RLEnv",
+    #                                            file_path = "requirements.txt")
     # rayEnv.docker.base_image = "mcr.microsoft.com/azureml/openmpi4.1.0-cuda11.1-cudnn8-ubuntu18.04:20220329.v1"
     
     experiment = Experiment(ws, experiment_name)
@@ -26,11 +30,11 @@ def main():
     aml_run_config_ml = RunConfiguration(communicator='OpenMpi')
     aml_run_config_ml.target = compute_target
     # aml_run_config_ml.docker = DockerConfiguration(use_docker=False)
-    aml_run_config_ml.node_count = 3
+    aml_run_config_ml.node_count = 2
     aml_run_config_ml.environment = rayEnv
 
     # rl_environment = "4x4grid_sumo_env"
-    script_name='run_tune_job.py' 
+    script_name='ray_tune_job.py' 
 
     command=[
         'python', script_name,
